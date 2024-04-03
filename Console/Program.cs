0e91e1310ebc710fa6c7e1ad85362039cff9e744
Alex Mickelson <alexmickelson96@gmail.com>
2024-04-03 13:57:41 -0600

    added user interface menu

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,8 +1,10 @@
 ﻿
 
-
+// the main function will run the infinite loop for the menu
+// each option will be handled by the functions in UserInterfaceFunctions.cs
 while (true)
 {
+  Console.Clear();
   Console.WriteLine(@"Welcome to the Movie Theatre. What would you like to do
 1. Load Movies and Showtimes from files
 2. View current and upcoming Movies
@@ -11,12 +13,36 @@ while (true)
 5. Add a Showtime to a current Movie
 6. Store Movies and Showtimes to files
 ");
-  // switch()
+  var userMenuChoice = Console.ReadLine();
+  switch (userMenuChoice)
+  {
+    case "1":
+      UserInterfaceFunctions.LoadMovies();
+      break;
+    case "2":
+      UserInterfaceFunctions.ViewCurrentAndUpcomingMovies();
+      break;
+    case "3":
+      UserInterfaceFunctions.ViewCurrentMovieSchedule();
+      break;
+    case "4":
+      UserInterfaceFunctions.RegisterNewMovie();
+      break;
+    case "5":
+      UserInterfaceFunctions.AddShowtimeToCurrentMovie();
+      break;
+    case "6":
+      UserInterfaceFunctions.PersistMoviesAndShowtimes();
+      break;
+    default:
+      Console.WriteLine("Invalid option");
+      break;
+  }
 
   Console.WriteLine("Press Any Key to Continue...");
+  Console.ReadKey(true);
   while (Console.KeyAvailable)
   {
     Console.ReadKey(true);
   }
-  Console.Clear();
 }
---
+++ Console/UserInterfaceFunctions.cs
@@ -0,0 +1,67 @@
+global using MovieTuple = (string title, int runLengthMinutes);
+global using ShowtimeTuple = (string title, System.DateTime showtime);
+using Shared;
+
+public static class UserInterfaceFunctions
+{
+  public static void LoadMovies()
+  {
+    Theatre.LoadMoviesAndShowtimesFromFiles();
+    Console.WriteLine("Movies Loaded!");
+  }
+
+  public static void ViewCurrentAndUpcomingMovies()
+  {
+
+    Console.WriteLine($"                                   Title | Runtime");
+    Console.WriteLine($"-----------------------------------------+--------");
+    foreach (MovieTuple movie in Theatre.Movies)
+    {
+      Console.WriteLine($"{movie.title,40} | {movie.runLengthMinutes}");
+    }
+  }
+
+  public static void ViewCurrentMovieSchedule()
+  {
+    // TODO:
+    // for each movie, show a list of showtimes
+
+  }
+
+  public static void RegisterNewMovie()
+  {
+    Console.WriteLine("Input the movie title");
+    string movieTitle = Console.ReadLine();
+
+    Console.WriteLine("Input the runtime in minutes");
+    string runtimeString = Console.ReadLine();
+
+    try
+    {
+      int runtime = int.Parse(runtimeString);
+      Theatre.AddMovie(movieTitle, runtime);
+      Console.WriteLine("Movie Added!");
+    }
+    catch (Exception e)
+    {
+      Console.WriteLine("Error creating movie " + e.Message);
+      Console.WriteLine("Please try again");
+
+      // recurse to have the user try to input a movie
+      RegisterNewMovie();
+    }
+  }
+
+  public static void AddShowtimeToCurrentMovie()
+  {
+    // TODO
+    // ask the user for the movie title and datetime
+    // use Theatre.AddShowtime() to add the showtime
+  }
+
+  public static void PersistMoviesAndShowtimes()
+  {
+    Theatre.SaveMoviesAndShowtimesToFiles();
+    Console.WriteLine("Movies Saved!");
+  }
+}
